da4adb4e1aa3553b1a09e57b3ebbaa57955fa07a
mithilesh <connectmk@hotmail.com>
2023-01-31 00:46:43 +0530

    deploy commit

--- backend/app.js
+++ backend/app.js
@@ -35,10 +35,10 @@ app.use('/api/v1', payment);
 
 
 if(process.env.NODE_ENV === 'PRODUCTION'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static('frontend/build'));
 
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/build','index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html'));
     }) 
 }
 
